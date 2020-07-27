e5fc745217ac59ea958da173b7602cc369006b3d
massmux <go@massmux.com>
2020-07-27 09:23:31 +0200

    fixes

--- server_configure.sh
+++ server_configure.sh
@@ -37,23 +37,23 @@ tee $WORKDIR/helper/add-nat-routing.sh <<EOF
 IPT="/sbin/iptables"
 IPT6="/sbin/ip6tables"
 
-IN_FACE="$IFACE"                   # NIC connected to the internet
+IN_FACE="\$IFACE"                   # NIC connected to the internet
 WG_FACE="wg0"                    # WG NIC
 SUB_NET="192.168.6.0/24"            # WG IPv4 sub/net aka CIDR
-WG_PORT="$IPORT"                  # WG udp port
+WG_PORT="\$IPORT"                  # WG udp port
 SUB_NET_6="fd42:42:42:42::/112"  # WG IPv6 sub/net
 
 ## IPv4 ##
-$IPT -t nat -I POSTROUTING 1 -s $SUB_NET -o $IN_FACE -j MASQUERADE
-$IPT -I INPUT 1 -i $WG_FACE -j ACCEPT
-$IPT -I FORWARD 1 -i $IN_FACE -o $WG_FACE -j ACCEPT
-$IPT -I FORWARD 1 -i $WG_FACE -o $IN_FACE -j ACCEPT
-$IPT -I INPUT 1 -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
+\$IPT -t nat -I POSTROUTING 1 -s \$SUB_NET -o \$IN_FACE -j MASQUERADE
+\$IPT -I INPUT 1 -i \$WG_FACE -j ACCEPT
+\$IPT -I FORWARD 1 -i \$IN_FACE -o \$WG_FACE -j ACCEPT
+\$IPT -I FORWARD 1 -i \$WG_FACE -o \$IN_FACE -j ACCEPT
+\$IPT -I INPUT 1 -i \$IN_FACE -p udp --dport \$WG_PORT -j ACCEPT
 ## IPv6
-$IPT6 -t nat -I POSTROUTING 1 -s $SUB_NET_6 -o $IN_FACE -j MASQUERADE
-$IPT6 -I INPUT 1 -i $WG_FACE -j ACCEPT
-$IPT6 -I FORWARD 1 -i $IN_FACE -o $WG_FACE -j ACCEPT
-$IPT6 -I FORWARD 1 -i $WG_FACE -o $IN_FACE -j ACCEPT
+\$IPT6 -t nat -I POSTROUTING 1 -s \$SUB_NET_6 -o \$IN_FACE -j MASQUERADE
+\$IPT6 -I INPUT 1 -i \$WG_FACE -j ACCEPT
+\$IPT6 -I FORWARD 1 -i \$IN_FACE -o \$WG_FACE -j ACCEPT
+\$IPT6 -I FORWARD 1 -i \$WG_FACE -o \$IN_FACE -j ACCEPT
 EOF
 
 tee $WORKDIR/helper/remove-nat-routing.sh <<EOF
@@ -61,23 +61,23 @@ tee $WORKDIR/helper/remove-nat-routing.sh <<EOF
 IPT="/sbin/iptables"
 IPT6="/sbin/ip6tables"          
  
-IN_FACE="$IFACE"                   # NIC connected to the internet
+IN_FACE="\$IFACE"                   # NIC connected to the internet
 WG_FACE="wg0"                    # WG NIC 
 SUB_NET="192.168.6.0/24"            # WG IPv4 sub/net aka CIDR
-WG_PORT="$IPORT"                  # WG udp port
+WG_PORT="\$IPORT"                  # WG udp port
 SUB_NET_6="fd42:42:42:42::/112"  # WG IPv6 sub/net
  
 # IPv4 rules #
-$IPT -t nat -D POSTROUTING -s $SUB_NET -o $IN_FACE -j MASQUERADE
-$IPT -D INPUT -i $WG_FACE -j ACCEPT
-$IPT -D FORWARD -i $IN_FACE -o $WG_FACE -j ACCEPT
-$IPT -D FORWARD -i $WG_FACE -o $IN_FACE -j ACCEPT
-$IPT -D INPUT -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
+\$IPT -t nat -D POSTROUTING -s \$SUB_NET -o \$IN_FACE -j MASQUERADE
+\$IPT -D INPUT -i \$WG_FACE -j ACCEPT
+\$IPT -D FORWARD -i \$IN_FACE -o \$WG_FACE -j ACCEPT
+\$IPT -D FORWARD -i \$WG_FACE -o \$IN_FACE -j ACCEPT
+\$IPT -D INPUT -i \$IN_FACE -p udp --dport \$WG_PORT -j ACCEPT
 # IPv6 rules
-$IPT6 -t nat -D POSTROUTING -s $SUB_NET_6 -o $IN_FACE -j MASQUERADE
-$IPT6 -D INPUT -i $WG_FACE -j ACCEPT
-$IPT6 -D FORWARD -i $IN_FACE -o $WG_FACE -j ACCEPT
-$IPT6 -D FORWARD -i $WG_FACE -o $IN_FACE -j ACCEPT
+\$IPT6 -t nat -D POSTROUTING -s \$SUB_NET_6 -o \$IN_FACE -j MASQUERADE
+\$IPT6 -D INPUT -i \$WG_FACE -j ACCEPT
+\$IPT6 -D FORWARD -i \$IN_FACE -o \$WG_FACE -j ACCEPT
+\$IPT6 -D FORWARD -i \$WG_FACE -o \$IN_FACE -j ACCEPT
 EOF
 
 echo "making helpers executable"
