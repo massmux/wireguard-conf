b147f693f4b60358dbca340a46782e994f20701e
massmux <go@massmux.com>
2020-08-01 12:01:56 +0200

    fixes

--- wgconf.sh
+++ wgconf.sh
@@ -56,7 +56,7 @@ select fav in "${opts[@]}"; do
 	    exit
             ;;
         "Client")
-	    echo "**to be run on CLIENT (first client)**"
+	    echo "**to be run on CLIENT, configure a peer **"
 	    read -p "Enter server ip [1.2.3.4]: " serverip
 	    serverip=${serverip:-1.2.3.4}
 	    read -p "Enter server port [41194]: " IPORT
@@ -73,12 +73,19 @@ select fav in "${opts[@]}"; do
 	    exit
             ;;
         "Set pubkey")
-	    echo "**to be run on SERVER (for adding first client)**"
+	    echo "**to be run on SERVER, adds a peer to the server **"
 	    read -p "Enter client's pubkey for the server: " clipubkey
 	    echo "Client's pub key: $clipubkey"
 	    read -p "Shall i set client's pub key? [enter] to continue or CTRL+C to exit" cont
-	    sed -i "s#PEER1PUB#$clipubkey#g" $WORKDIR/wg0.conf
-	    sed -i "s/#SaveConfig/SaveConfig=true/g" $WORKDIR/wg0.conf
+	    ##sed -i "s#PEER1PUB#$clipubkey#g" $WORKDIR/wg0.conf
+	    ##sed -i "s/#SaveConfig/SaveConfig=true/g" $WORKDIR/wg0.conf
+	    echo "adding peer to server"
+tee -a $WORKDIR/wg0.conf <<EOF
+[Peer]
+PublicKey = $clipubkey
+AllowedIPs = 192.168.6.0/24,fd42:42:42:42::/112
+EOF
+	    echo "restarting service"
 	    systemctl restart wg-quick@wg0.service
             echo "Complete"
             break
